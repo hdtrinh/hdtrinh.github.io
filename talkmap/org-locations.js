71021a7d879121bf5ba418b4521563d7a397f810
Hoang Duy Trinh <trinhhoangduy@gmail.com>
2020-09-06 22:42:38 +0200

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,37 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Split, Croatia",
+    43.5116383,
+    16.4399659
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Boston, Massachusetts, USA",
+    42.3602534,
+    -71.0582912
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Paris, France",
+    48.8566969,
+    2.3514616
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Strathclyde University, Glasgow, UK",
+    55.8605911,
+    -4.2409692
+  ],
+  [
+    "Montreal, Canada",
+    45.4972159,
+    -73.6103642
+  ],
+  [
+    "CTTC, Barcelona, Spain",
+    41.27492475,
+    1.9877285500000015
+  ],
+  [
+    "Sitges, Spain",
+    41.2366707,
+    1.8228136
   ]
 ];
